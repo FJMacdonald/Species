f286643932a60c90980eba005bff03b8098657c5
Francesca Macdonald <104513846+Francesca-Quevita@users.noreply.github.com>
2023-08-29 09:25:20 -0400

    Initial Commit

---
+++ Species/SpeciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeciesApp.swift
+//  Species
+//
+//  Created by Francesca MACDONALD on 2023-08-29.
+//
+
+import SwiftUI
+
+@main
+struct SpeciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
